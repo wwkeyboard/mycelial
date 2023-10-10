24c9f72e97add13f7fdf1886723528e347ae4a86
Alex Wilson <alex@mycelial.com>
2023-10-10 11:01:28 -0400

    add rewrite to nextjs config

--- console/next.config.js
+++ console/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  // output: "export",
+  rewrites: async () => {
+    return {
+      fallback: [
+        {
+          source: "/:path*",
+          destination: `http://localhost:8080/:path*`,
+        },
+      ],
+    };
+  },
 };
 
 module.exports = nextConfig;
